5232e92e2f8a741d3a26643f51e4486e1f0941e1
Dauda Lawal <64631869+Daslaw@users.noreply.github.com>
2022-04-03 02:40:47 +0100

    Wella Week 1 Task

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace week1task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Dauda Lawal, I am in Your Track of Wella School");
+            Console.WriteLine("I am an IT enthusiast with interest in software development. It’s my belief that internet is the best way to connect people together. Learning software development will give me an opportunity to contribute my quota to development of Africa Ecosystem");
+        }
+    }
+}
